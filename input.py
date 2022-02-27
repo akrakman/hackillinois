bce51364ec8fc3b79893928bc6d67d376e61e96b
Aden Krakman <adenkrakman@wirelessprv-10-195-91-34.near.illinois.edu>
2022-02-27 00:03:26 -0600

    csv

--- input.py
+++ input.py
@@ -10,4 +10,16 @@ while(yes == 'y'):
 
 with open('output.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
-    
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            #get average grade
+            teacher = row[6]
+            course = row[5]
+            grade = []
+            print(teacher)
+            for i in range(7, 17):
+                grade.append(row[i])
