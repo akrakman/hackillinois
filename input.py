7ab6566a0cf7574a29d72c46b175df11ccd420c3
Aden Krakman <adenkrakman@wirelessprv-10-195-91-34.near.illinois.edu>
2022-02-27 00:27:04 -0600

    s

--- input.py
+++ input.py
@@ -1,10 +1,11 @@
+from numpy import average
 import pandas as pd
 import csv
 
 yes = 'y'
 classes = []
 while(yes == 'y'):
-    name, num, prof = input("Enter course, number, and professor name: ").split(",")
+    name, num, prof = input("Enter course, number, and professor name: ").split(".")
     classes.append((name, num, prof))
     yes = input("Enter 'y' if you want to add another class: ")
 
@@ -12,14 +13,18 @@ with open('output.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     line_count = 0
     for row in csv_reader:
+        ##print(row[7])
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
+            #print(f'Column names are {", ".join(row)}')
             line_count += 1
-        else:
+        elif row[7] == prof:
             #get average grade
-            teacher = row[6]
-            course = row[5]
-            grade = []
-            print(teacher)
-            for i in range(7, 17):
-                grade.append(row[i])
+            course = row[6]
+            print(prof, course)
+            #average = row[23]
+            break
+        #NOW WE HAVE AVERAGE GPA, PROF, COURSE!!
+            
+                
+
+
