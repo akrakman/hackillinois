1498148743d748476d544d009755f8abcf6519be
Aden Krakman <adenkrakman@wirelessprv-10-195-91-34.near.illinois.edu>
2022-02-26 22:49:31 -0600

    minor edits

--- RateProf.py
+++ RateProf.py
@@ -13,7 +13,7 @@ class RateMyProfScraper:
             num_of_prof = self.GetNumOfProfessors(self.UniversityId)
             num_of_pages = math.ceil(num_of_prof / 20)
             i = 1
-            while (i <= num_of_pages):# the loop insert all professor into list
+            while (i <= num_of_pages): # the loop insert all professor into list
                 page = requests.get("http://www.ratemyprofessors.com/filter/professor/?&page=" + str(
                     i) + "&filter=teacherlastname_sort_s+asc&query=*%3A*&queryoption=TEACHER&queryBy=schoolId&sid=" + str(
                     self.UniversityId))
