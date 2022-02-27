f85b576591ceac825a9c50c1db0d1040de29f016
Arnav Rawat <rawat.arnav@gmail.com>
2022-02-26 23:10:26 -0600

    Brain dead rn

--- sentimental_analysis.py
+++ sentimental_analysis.py
@@ -1,10 +1,15 @@
 from textblob import TextBlob
 
 class ScaleUtilities:
-
+    average = 0
+    number = 0
     def get_sentiment_of(string):
-        return TextBlob(string).sentiment.polarity * 5
+        polarity = TextBlob(string).sentiment.polarity * 5
+        number++
+        average += polarity
+        return polarity
 
     def get_subjectivity_of(string):
-        return TextBlob(string).sentiment.subjectivity * 5
+        subjectivity = TextBlob(string).sentiment.subjectivity * 5
+        return
     def
