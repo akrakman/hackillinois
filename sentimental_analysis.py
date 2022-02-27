bb988a6aef09a48653e8aa6dfa8fae310a6bc561
Aden Krakman <adenkrakman@wirelessprv-10-195-91-34.near.illinois.edu>
2022-02-26 23:11:08 -0600

    edit

--- sentimental_analysis.py
+++ sentimental_analysis.py
@@ -3,8 +3,8 @@ from textblob import TextBlob
 class ScaleUtilities:
 
     def get_sentiment_of(string):
+        
         return TextBlob(string).sentiment.polarity * 5
 
     def get_subjectivity_of(string):
         return TextBlob(string).sentiment.subjectivity * 5
-    def
